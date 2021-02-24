ccd4c729c59876af98a43defd2611782f3eff03a
Viktor Gubarevich <gubarew.v@gmail.com>
2021-02-24 21:03:13 +0300

    feet: add index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        let min = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] < min) min = array[i];
+        }
+        return min;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0
+    }
+    return array.reduce((max, currentValue) => currentValue > max ? max = currentValue : max)
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0
+    }
+    return array.reduce((a, b) => (a + b)) / array.length;
+};
